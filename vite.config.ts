534a9bd18cee338bce002e8c0c2b9e23a859f69b
Alex-La <55190911+Alex-La@users.noreply.github.com>
2023-03-04 02:28:37 +0300

    Configure environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig, loadEnv} from 'vite'
+import _ from 'lodash'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    host: true,
-  },
-  plugins: [react()],
-});
+import react from '@vitejs/plugin-react'
+import TsconfigPaths from 'vite-tsconfig-paths'
+import PluginRewriteAll from 'vite-plugin-rewrite-all'
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd())
+
+  const isEnvEmpty = _.isEmpty(env)
+  if (isEnvEmpty) throw new Error(`Please, provide ${mode} env config.`)
+
+  return {
+    server: {
+      port: 3000,
+      host: true,
+    },
+    plugins: [react(), TsconfigPaths(), PluginRewriteAll()],
+  }
+})
